36112ffa7cd37afa126fec0ad73b08c278fe474f
Guilherme Garcia <guilherme4garcia@gmail.com>
2019-09-14 17:45:41 -0300

    Delete #10.py

--- #10.py
+++
@@ -1,5 +0,0 @@
-cigarrosdia = int(input('Qual a quantidade de cigarros fumados por dia? '))
-cigarrosano = int(input('Por quantos anos você fumou? '))
-total = cigarrosdia * (cigarrosano * 365)
-diasperdidos = (total * 10) / 1440
-print(f'Dias perdidos de vida: {diasperdidos:.0f}')
