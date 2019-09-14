9998d8af82965ad0cd0631d983bfaff532a89aac
Guilherme Garcia <guilherme4garcia@gmail.com>
2019-09-14 17:46:53 -0300

    Add files via upload

---
+++ Lista I/#03 Dias, horas, minutos, segundos.py
@@ -0,0 +1,8 @@
+dias = int(input('Digite uma quantidade de dias: '))
+horas = float(input('Digite uma quantidade de horas: '))
+minutos = float(input('Digite uma quantidade de minutos: '))
+segundos = float(input('Digite uma quantidade de segundos: '))
+
+total = (dias * 86400) + (horas * 3600) + (minutos * 60) + segundos
+
+print(f'{dias} dias, {horas:.2f} horas, {minutos:.2f} minutos e {segundos:.2f} segundos, equivale a {total:.2f} segundos.')
---
+++ Lista I/#07.py
@@ -0,0 +1,3 @@
+c = float(input('Digite uma temperatura em °C: '))
+f = (9 * c /5) + 32
+print(f'{f:.1f}°F')
