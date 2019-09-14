7d0b23fa9c0e7685495aeabcffa10a70c27c466f
Guilherme Garcia <guilherme4garcia@gmail.com>
2019-09-14 17:45:15 -0300

    Delete #05 Descontos.py

--- #05 Descontos.py
+++
@@ -1,7 +0,0 @@
-x = float(input('Qual é o preço do produto? '))
-d = int(input('Qual a porcentagem do desconto? '))
-
-desconto = (d * x) / 100
-resultado = x - desconto
-
-print(f'O produto que custava R${x:.2f}, na promoção com desconto de {d}% vai custar R${resultado:.2f}. Você economizou R${desconto:.2f}')
