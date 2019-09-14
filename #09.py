f8747e270dad02ba3155831461d7cbe58d7c6d5b
Guilherme Garcia <guilherme4garcia@gmail.com>
2019-09-14 17:45:49 -0300

    Delete #09.py

--- #09.py
+++
@@ -1,6 +0,0 @@
-distancia = float(input('Qual a distância percorrida em km: '))
-dias = float(input('Qual a quantidade de dias que o carro foi alugado: '))
-valordias = 60.00 * dias
-valordistancia = 0.15 * distancia
-total = valordias + valordistancia
-print(f'R${total:.2f}')
