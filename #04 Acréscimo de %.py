726929111e7bfbde9b9454c9ee64bb854cd2cd11
Guilherme Garcia <guilherme4garcia@gmail.com>
2019-09-14 17:45:07 -0300

    Delete #04 Acréscimo de %.py

--- #04 Acréscimo de %.py
+++
@@ -1,5 +0,0 @@
-s = float(input('Digite o valor do salário: '))
-p = float(input('Digite a porcentagem de aumento: '))
-x = p / 100 * s
-t = s + x
-print(f'Com um acréscimo de {p}%, o salário passará de R${s:.2f} para R${t:.2f}. Uma diferença de R${x:.2f}. ')
